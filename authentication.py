ea1d0e7816068f33d374a6d2c99640e53f34a253
souraverdodia <153484093+souraverdodia@users.noreply.github.com>
2024-11-17 22:58:07 +0530

    Create authentication.py

---
+++ authentication.py
@@ -0,0 +1,15 @@
+import pickle
+from pathlib import path 
+
+import streamlit_authenticator as stauth
+
+names = ["Sourav Verdodia" , "Sahil" , "Saurabh Sinha"]
+usernames = ["souraverdodia" , "sahiloraon" , "saurabhsinha"]
+passwords = ["souraverdodia" , "sahiloraon" , "saurabhsinha"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pw.pk1"
+with file_path.open("wb") as file: 
+  pickle.dump(hashed_passwords , file)
+  

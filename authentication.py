42d3a85b75551b2c107aac31c8224f6a5ca7c58a
souraverdodia <153484093+souraverdodia@users.noreply.github.com>
2024-11-18 12:34:48 +0530

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -9,7 +9,7 @@ passwords = ["souraverdodia" , "sahiloraon" , "saurabhsinha"]
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
-file_path = Path(__file__).parent / "hashed_pw.pk1"
+file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("wb") as file: 
   pickle.dump(hashed_passwords , file)
   
